b9b48421d8fcdf33ac981bdef0a09b1418573c79
Marlon Nardi <marlonnardi@hotmail.com>
2021-11-01 18:32:58 -0400

    * Add support for TextBold in AddText

--- ksVirtualListView.pas
+++ ksVirtualListView.pas
@@ -531,6 +531,7 @@ type
     function IsItemVisible(AViewPort: TRectF): Boolean;
     function AddText(x, y: single; AText: string): TksVListItemTextObject; overload;
     function AddText(x, y: single; AText: string; AFontColor: TAlphaColor; AFontSize: integer): TksVListItemTextObject; overload;
+    function AddTextBold(x, y: single; AText: string; AFontColor: TAlphaColor; AFontSize: integer): TksVListItemTextObject; overload;
     function AddText(x, y, AWidth: single; AText: string): TksVListItemTextObject; overload;
     function AddDetailText(y: single; AText: string): TksVListItemTextObject; overload;
     function AddImage(x, y, AWidth, AHeight: single; ABitmap: TBitmap): TksVListItemImageObject;
@@ -1125,6 +1126,15 @@ begin
   Result.FChecked := AChecked;
   FCanSelect := False;
   FObjects.Add(Result);
+end;
+
+function TksVListItem.AddTextBold(x, y: single; AText: string;
+  AFontColor: TAlphaColor; AFontSize: integer): TksVListItemTextObject;
+begin
+  Result := AddText(x, y, AText);
+  Result.TextSettings.FontColor := AFontColor;
+  Result.TextSettings.Font.Size := AFontSize;
+  Result.TextSettings.Font.Style := [TFontStyle.fsBold];
 end;
 
 function TksVListItem.AddText(x, y: single; AText: string;
